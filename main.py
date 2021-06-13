a8a230cdf953d3824ee26e4cde08e8c985d80f84
Lincoln <zhoulang731@gmail.com>
2021-06-13 09:10:55 +0800

    Optimizing code structure, add TODO

--- main.py
+++ main.py
@@ -23,54 +23,65 @@ import numpy as np
 from config import ROCK_MIN_SIZE
 import utilities as utl
 
-original_image = cv2.imread("sample/Mars_Perseverance_NLF_0102_0676000820_801ECM_N0040372NCAM03101_01_295J.png")
-original_image = cv2.GaussianBlur(original_image, (5, 5), 2)
-visualized_image = original_image.copy()
 
-with utl.Timer("Mean shift segmenting..."):
-    (segmented_image, labels_image, number_regions) = pms.segment(original_image, spatial_radius=6, range_radius=4.5,
-                                                                  min_density=ROCK_MIN_SIZE)
+def rock_extraction(image: str):
+    # TODO: Using 3D point cloud reconstructed from stereo camera to extract rocks more precisely
+    original_image = cv2.imread(image)
+    original_image = cv2.GaussianBlur(original_image, (5, 5), 2)
+    visualized_image = original_image.copy()
 
-cv2.imwrite("output/seg.png", segmented_image)
-cv2.imwrite("output/label.png", labels_image)
-print(f"Regions segmented: {number_regions}")
+    with utl.Timer("Mean shift segmenting..."):
+        (segmented_image, labels_image, number_regions) = pms.segment(original_image, spatial_radius=6,
+                                                                      range_radius=4.5,
+                                                                      min_density=ROCK_MIN_SIZE)
 
-max_size = 0
+    cv2.imwrite("output/seg.png", segmented_image)
+    cv2.imwrite("output/label.png", labels_image)
+    print(f"Regions segmented: {number_regions}")
 
-with utl.Timer("Shape analyzing..."):
-    for i in range(number_regions):
-        # Get a boolean image where True pixels belongs to the region
-        region_mask = (labels_image == i)
-        region_size = np.count_nonzero(region_mask)
+    max_size = 0
 
-        if region_size > max_size:
-            max_size = region_size
+    with utl.Timer("Shape analyzing..."):
+        for i in range(number_regions):
+            # Get a boolean image where True pixels belongs to the region
+            region_mask = (labels_image == i)
+            region_size = np.count_nonzero(region_mask)
 
-        region_location = np.nonzero(
-            region_mask)  # Gets non-zero values' locations, in the form of tuple(np.array(xi), np.array(yi))
+            if region_size > max_size:
+                max_size = region_size
 
-        edge = utl.edge_extraction(region_mask)
-        edge_points = np.transpose(np.nonzero(edge))
+            region_location = np.nonzero(
+                region_mask)  # Gets non-zero values' locations, in the form of tuple(np.array(xi), np.array(yi))
 
-        params = utl.ellipse_model_fitting(edge_points)
+            edge = utl.edge_extraction(region_mask)
+            edge_points = np.transpose(np.nonzero(edge))
 
-        fit_result = utl.ellipse_filtering(i, params)
+            params = utl.ellipse_model_fitting(edge_points)
 
-        if not fit_result:
-            continue
+            fit_result = utl.ellipse_filtering(i, params)
 
-        print(f"Ellipse fitting succeeded for area {i}")  # Print method here is for inspection use, will be deprecated in release
+            if not fit_result:
+                continue
 
-        visualized_image[tuple(edge_points.transpose())] = [0, 0, 255]
+            print(f"Ellipse fitting succeeded for area {i}")  # Print method here is for inspection use, will be deprecated in release
 
-        point_x = region_location[0][region_size // 2]
-        point_y = region_location[1][region_size // 2]
-        position = (point_y, point_x)
-        visualized_image = cv2.putText(visualized_image, str(region_size), position, cv2.FONT_HERSHEY_SIMPLEX, 0.35,
-                                       (255, 0, 0), 1)
+            visualized_image[tuple(edge_points.transpose())] = [0, 0, 255]
 
-print(f"Max region size is {max_size}, defining it as terrain...")
+            point_x = region_location[0][region_size // 2]
+            point_y = region_location[1][region_size // 2]
+            position = (point_y, point_x)
+            visualized_image = cv2.putText(visualized_image, str(region_size), position, cv2.FONT_HERSHEY_SIMPLEX, 0.35,
+                                           (255, 0, 0), 1)
 
-cv2.imwrite("output/visualized.png", visualized_image)
-cv2.imshow("vis", visualized_image)
-cv2.waitKey()
+    print(f"Max region size is {max_size}, defining it as terrain...")
+
+    cv2.imwrite("output/visualized.png", visualized_image)
+    cv2.imshow("vis", visualized_image)
+    cv2.waitKey()
+
+    return  # TODO: Return numpy.ndarray like descriptor for all rocks extracted
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    rock_extraction("sample/Mars_Perseverance_NLF_0102_0676000820_801ECM_N0040372NCAM03101_01_295J.png")
