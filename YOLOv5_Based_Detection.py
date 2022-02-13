b4692282ff7e78df5c3f7b3f08c2a0a4e5504f4b
Lincoln Zhou <zhoulang731@gmail.com>
2022-02-13 21:36:43 +0800

    Add YOLOv5 based crater detection method, this method exceeds all previous algorithms in both speed and performance.
    Signed-off-by: Lincoln <zhoulang731@gmail.com>

---
+++ YOLOv5_Based_Detection.py
@@ -0,0 +1,59 @@
+"""
+This script containing method for crater detection using YOLOv5 framework
+Copyright 2022 Lincoln Zhou, zhoulang731@tongji.edu.cn
+"""
+
+
+import torch
+
+import os
+
+
+def crater_detection(images, weight_path: str, yolov5_path: str):
+    """
+    Detect crater using trained YOLOv5 model file
+    :param images: list of image paths (str)
+    :param weight_path: YOLOv5 model parameters file's path
+    :param yolov5_path: YOLOv5 folder path
+    :return: [[[(center_x, center_y), (width, height)], ..], ..]
+    """
+    assert len(images) >= 1, 'No valid images'
+
+    try:
+        model = torch.hub.load(yolov5_path, 'custom', path=weight_path, source='local')
+        model.conf = 0.5
+    except:
+        print("Model file cannot be loaded, stop.")
+        return
+
+    detection_result = []
+
+    for image in images:
+        output = model(image)
+
+        result = output.pandas().xyxy[0]
+        # print(result)
+
+        current_image_result = []
+
+        for row in result.itertuples(index=True, name='Pandas'):
+            center_x = int((row.xmin + row.xmax) / 2)
+            center_y = int((row.ymin + row.ymax) / 2)
+
+            width = row.xmax - row.xmin
+            height = row.ymax - row.ymin
+
+            current_image_result.append([(center_x, center_y), (width, height)])
+
+        detection_result.append(current_image_result)
+
+    return detection_result
+
+
+if __name__ == '__main__':
+    test_folder_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater\test\images"
+    best_pt_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater\runs\train\exp5\weights\best.pt"
+    yolov5_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater"
+    test_images = os.listdir(test_folder_path)
+
+    print(crater_detection([os.path.join(test_folder_path, x) for x in test_images], weight_path=best_pt_path, yolov5_path=yolov5_path))
