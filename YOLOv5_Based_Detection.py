0ee980d05ff8fd676ee2aa7833b7e958ede7fc82
Lincoln Zhou <zhoulang731@gmail.com>
2022-03-15 01:14:04 +0800

    Add rock detection method based on YOLOv5.
    Signed-off-by: Lincoln <zhoulang731@gmail.com>

--- YOLOv5_Based_Detection.py
+++ YOLOv5_Based_Detection.py
@@ -53,7 +53,50 @@ def crater_detection(images, weight_path: str, yolov5_path: str):
     return detection_result_loc, detection_result_parm
 
 
+def rock_detection(image: str, weight_path: str, yolov5_path: str, image_size: int = 1280):
+    """
+    Detect crater using trained YOLOv5 model file
+    :param image: image path (str)
+    :param weight_path: YOLOv5 model parameters file's path
+    :param yolov5_path: YOLOv5 folder path
+    :param image_size: Image size for YOLO detection, default value suits most close-view photos, for large DOM image please choose a suitable value (preferably 2^n)
+    :return: [[[center_x, center_y]]]
+    """
+
+    try:
+        model = torch.hub.load(yolov5_path, 'custom', path=weight_path, source='local')
+        model.conf = 0.45
+    except:
+        print("Model file cannot be loaded, stop.")
+        return
+
+    detection_result_loc = []
+
+    result = model(image, size=image_size)
+    result.print()
+    result.show()
+
+    result = result.pandas().xyxy[0]
+    print(result)
+
+    current_image_result_loc = []
+
+    for row in result.itertuples(index=True, name='Pandas'):
+        center_x = int((row.xmin + row.xmax) / 2)
+        center_y = int((row.ymin + row.ymax) / 2)
+
+        current_image_result_loc.append([center_x, center_y])
+
+    detection_result_loc.append(current_image_result_loc)
+
+    return detection_result_loc
+
+
 if __name__ == '__main__':
+    rock_detection('sample/Explorer_HD1080_SN22734452_09-07-19.png', weight_path='sample/rock_v1.pt',
+                   yolov5_path=r"C:\Users\Lincoln\Development\ML\yolov5_rock", image_size=1920)
+    exit()
+
     test_folder_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater\test\images"
     best_pt_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater\runs\train\exp5\weights\best.pt"
     yolov5_path = r"C:\Users\Lincoln\Development\ML\yolov5_crater"
